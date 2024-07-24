8a9980259bbc270fb3cefc7dd1cabc7b68fb9865
November7 <m.kowalski.nov7@outlook.com>
2024-07-24 23:08:03 +0200

    Update

--- Lesson 4/Program.cs
+++ Lesson 4/Program.cs
@@ -1,13 +1,13 @@
-﻿int fun(int a, int b)
-{
-    return a + b;
-}
+﻿// int fun(int a, int b)
+// {
+//     return a + b;
+// }
 
-int fun(int a, int b, int c)
-{
-    return a + b + c;
-}
+// int fun(int a, int b, int c)
+// {
+//     return a + b + c;
+// }
 
 
-Console.WriteLine(fun(1,2));
-Console.WriteLine(fun(1,2,3));
+// Console.WriteLine(fun(1,2));
+// Console.WriteLine(fun(1,2,3));
