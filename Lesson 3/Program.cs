9b8feffabd8fe97b0a37562ab79c4f55b9de5b41
November7 <m.kowalski.nov7@outlook.com>
2024-09-30 17:05:37 +0200

    Update

--- Lesson 3/Program.cs
+++ Lesson 3/Program.cs
@@ -84,10 +84,11 @@ foreach(int i in T)
     Console.WriteLine(i);
 
 
-string[] vowels = ["a", "e", "i", "o", "u"];
-string[] consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
-                       "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"];
-string[] alphabet = [.. vowels, .. consonants, "y"];
-
-foreach(string s in alphabet)
-    Console.WriteLine(s);
+int[] row0 = [1, 2, 3];
+int[] row1 = [4, 5, 6];
+int[] row2 = [7, 8, 9];
+int[] single = [.. row0, .. row1, .. row2];
+foreach (var element in single)
+{
+    Console.Write($"{element}, ");
+}
