6d6ec9d6d9a74a051fad12f2c70817435747e966
November7 <m.kowalski.nov7@outlook.com>
2024-10-01 11:17:02 +0200

    Update

--- Lesson 11/Program.cs
+++ Lesson 11/Program.cs
@@ -1,10 +1,1 @@
-﻿using System.ComponentModel.Design;
-
-int[] t = {1,2,5,3,2,4,65,7,4,3,2,2,24,6,7};
-
-var result = from item in t where item != 1 select item;
-
-foreach(var it in result)
-{
-    Console.WriteLine(it);
-}
+﻿
--- Lesson 7/Program.cs
+++ Lesson 7/Program.cs
@@ -1,14 +1,1 @@
-﻿using System;
-using System.Net.Security;
-
-namespace MyApp7 // Note: actual namespace depends on the project name.
-{
-    partial class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            hello();
-        }
-    }
-}
+﻿
