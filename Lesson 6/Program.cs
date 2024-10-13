0d2efb121b31091dffdd15748fbc6be1d1b175e6
November7 <m.kowalski.nov7@outlook.com>
2024-10-13 23:00:33 +0200

    Update

--- Lesson 6/Program.cs
+++ Lesson 6/Program.cs
@@ -1,1 +1,99 @@
-﻿
+﻿// class App
+//  {
+//     static void Main()
+//     {
+//         //Instrukcja warunkowa:
+
+//         int nLiczba = 5;
+
+
+//         if (nLiczba % 2 == 0)
+//         {
+//             Console.WriteLine("Liczba jest parzysta.");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Liczba jest nieparzysta.");
+//         }
+
+//         //Instrukcja warunkowa z wykorzystaniem składni zagnieżdżonych:
+
+        
+//         if (nLiczba % 2 == 0)
+//         {
+//             Console.WriteLine("Liczba jest parzysta.");
+//         }
+//         else
+//         {
+//             if (nLiczba % 3 == 0)
+//             {
+//                 Console.WriteLine("Liczba jest podzielna przez 3.");
+//             }
+//             else
+//             {
+//                 Console.WriteLine("Liczba nie jest parzysta ani podzielna przez 3.");
+//             }
+//         }
+
+//         if (nLiczba % 2 == 0)
+//         {
+//             Console.WriteLine("Liczba jest parzysta.");
+//         }
+//         else if (nLiczba % 3 == 0)
+//         {
+//             Console.WriteLine("Liczba jest podzielna przez 3.");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Liczba nie jest parzysta ani podzielna przez 3.");
+//         }
+
+//         //Instrukcja warunkowa z wykorzystaniem operatora warunkowego (operator ternarny):
+
+//         Console.WriteLine((nLiczba % 2 == 0 && nLiczba % 3 == 0) ? "Liczba jest parzysta i podzielna przez 3." : "Liczba nie jest parzysta ani podzielna przez 3.");        
+
+
+//     }
+// }
+class App
+ {
+    static void Main()
+    {
+        int x = 123;
+        switch (x)
+        {
+            case 1:
+                Console.WriteLine("x = 1");
+                break;
+            case 2:
+                Console.WriteLine("x = 2");
+                break;
+            case 3:
+                Console.WriteLine("x = 3");
+                break;
+            default:
+                Console.WriteLine("x nie jest 1, 2, lub 3");
+                break;
+        }
+
+
+        string s = "123";
+
+        switch (s)
+        {
+            default:
+                Console.WriteLine("s nie jest 1, 2, lub 3");
+                break;
+            case "1":
+                Console.WriteLine("s = 1");
+                break;
+            case "2":
+                Console.WriteLine("s = 2");
+                break;
+            case "3":
+                Console.WriteLine("s = 3");
+                break;
+        }
+    }
+
+}
--- Lesson 7/Program.cs
+++ Lesson 7/Program.cs
@@ -1,1 +1,74 @@
-﻿
+﻿class App
+{
+
+    static void Main()
+    {
+        //Pętla for
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"Iteracja: {i}");
+        }
+
+        //Pętla foreach
+        
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        
+        foreach (int number in numbers)
+        {
+            Console.WriteLine($"Liczba: {number}");
+        }
+
+        string s = "Ala ma kota";
+
+        foreach (char c in s)
+        {
+            Console.WriteLine($"Znak: {c}");
+        }
+
+        //Pętla while
+        
+        int j = 0;
+        
+        while (j < 10)
+        {
+            Console.WriteLine($"Iteracja: {j}");
+            j++;
+        }
+
+        //Pętla do-while
+        
+        int k = 0;
+        
+        do
+        {
+            Console.WriteLine($"Iteracja: {k}");
+            k++;
+        } while (k < 10);
+
+        //Instrukcja break
+        
+        for (int i = 0; i < 10; i++)
+        {
+            if (i == 5)
+            {
+                break;
+            }
+            Console.WriteLine($"Iteracja: {i}");
+        }
+
+        //Instrukcja continue
+        
+        for (int i = 0; i < 10; i++)
+        {
+            if (i % 2 == 0)
+            {
+                continue;
+            }
+            
+            Console.WriteLine($"Liczba: {i}");
+            
+        }
+        
+    }
+}
