e9476e2b6826b5b1b7a3dc77cbcec0b8fd9f087b
November7 <m.kowalski.nov7@outlook.com>
2024-10-27 21:17:44 +0100

    Update

--- Lesson 9/Program.cs
+++ Lesson 9/Program.cs
@@ -2,9 +2,18 @@
 {
     static void Main()
     {
-        Random random = new Random();
-        //Lista
-        List<int> Lista1 = new List<int>();
+        // Random random1 = new();
+        // Console.WriteLine(random1.Next());
+        // var random2 = new Random();
+        // Console.WriteLine(random2.Next());
+
+        Random random = new();
+        // Lista
+
+        Console.WriteLine("Lista");
+        List<int> Lista1 = new();
+        // List<int> Lista1 = new List<int>();
+
         for (int i = 0; i < 10; i++)
         {
             Lista1.Add(random.Next(10));
@@ -27,7 +36,8 @@
         Lista1.Insert(0,123);
         Console.WriteLine(string.Join(", ", Lista1));
 
-        List<object> Lista2 = new List<object>();
+        // List<object> Lista2 = new List<object>();
+        List<object> Lista2 = new();
         Lista2.Add(1);
         Lista2.Add(2.5);
         Lista2.Add("tekst");
@@ -39,80 +49,98 @@
         }
         Console.WriteLine();
 
-        //Krotka
-        (int, float, string) Krotka1 = (1,3.14f,"tekst");
-        Console.WriteLine($"{Krotka1.Item1}, {Krotka1.Item2}, {Krotka1.Item3}");
-        var Krotka1b = (1,2,3,4);
-        Console.WriteLine($"{Krotka1b.Item1}, {Krotka1b.Item2}, {Krotka1b.Item3}");
-       
-        var Krotka2 = (a:1, b:3.14f, c:"tekst");
-        Console.WriteLine($"{Krotka2.a}, {Krotka2.b}, {Krotka2.c}");
+        List<object> Lista3 = [1,2,3,"randomowy tekst",3.123,2,4,5,5];
 
-        (int a, float b, string c) Krotka3 = (1, 3.14f, "tekst");
-        Console.WriteLine($"{Krotka3.a}, {Krotka3.b}, {Krotka3.c}");
-       
-        var Krotka4 = (1,3.14,"tekst");
-        Console.WriteLine($"{Krotka4.Item1}, {Krotka4.Item2}, {Krotka4.Item3}");
-
-        var Krotka5 = new Tuple<int,float,string>(1,3.14f,"tekst");
-        Console.WriteLine($"{Krotka5.Item1}, {Krotka5.Item2}");
-
-        Console.WriteLine($"{Krotka1.GetType()}\n {Krotka1b.GetType()}\n {Krotka2.GetType()}\n{Krotka3.GetType()}\n{Krotka4.GetType()}\n{Krotka5.GetType()}");
-
-        Console.WriteLine($"{Krotka1 == Krotka2}");
-        // Console.WriteLine($"{Krotka1 == Krotka5}"); //błąd
-
-        //Zbiór
-        #pragma warning disable IDE0028
-        HashSet<int> Zbior1 = new HashSet<int> {7, 2, 3, 4, 5};
-        HashSet<int> Zbior1b = new () {7, 2, 3, 4, 5};
-
-        Zbior1.Add(5);
-        Zbior1.Add(5);
-        Zbior1.Add(6);
-        Zbior1.Remove(3);
-
-        foreach (var item in Zbior1)
+        foreach (var item in Lista3)
         {
             Console.Write($"{item}, ");
         }
         Console.WriteLine();
 
-        HashSet<string> Zbior2 = ["ala","ma","kota"];
-        // Console.WriteLine($"{Zbior1},{Zbior2}");
+        //Klasa Tuple
+        Console.WriteLine("Klasa Tuple");
+        Tuple<int, string> Krotka1 = new Tuple<int, string>(1, "tekst");
+        Tuple<int, string, float> Krotka2 = new (1, "tekst", 3.5f);
 
-        foreach (var item in Zbior2)
-        {
-            Console.Write($"{item}, ");
-        }
-        Console.WriteLine();
+        Console.WriteLine($"{Krotka1.Item1}, {Krotka1.Item2}");
+        Console.WriteLine($"{Krotka2.Item1}, {Krotka2.Item2}, {Krotka2.Item3}");
+        Console.WriteLine($"{Krotka1.GetType()}\n{Krotka2.GetType()}\n");
 
-        //Słownik
-        // #pragma warning disable IDE0090
-        Dictionary<string, int> dict1 = new Dictionary<string, int>
-        {
-            { "Alice", 30 },
-            { "Bob", 25 },
-            { "Charlie", 35 }
-        };
+        // Krotka1.Item1 = 123; //błąd - elementy tylko do odczytu
 
-        Dictionary<string, int> dict2 = new() { { "Alice", 30 }, { "Bob", 25 }};
+        //Krotka wartości (Value Tuple)
+        Console.WriteLine("Value Tuple");
+        
+        (int, float, string) WarKrotka1 = (1, 3.14f, "tekst");
+        Console.WriteLine($"{WarKrotka1.Item1}, {WarKrotka1.Item2}, {WarKrotka1.Item3}");
 
-        // Dodawanie elementów do słownika
-        dict1.Add("David", 40);
-        dict2.Add("David", 40);
+        var WarKrotka2 = (1, 2, 3, 4);
+        Console.WriteLine($"{WarKrotka2.Item1}, {WarKrotka2.Item2}, {WarKrotka2.Item3}");
 
-        // Wyświetlanie zawartości słownika
-        foreach (var item in dict1)
-        {
-            Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
-        }
+        WarKrotka1.Item1 = 123;
+        WarKrotka2.Item2 = 321;
+       
+        var WarKrotka3 = (a:1, b:3.14f, c:"tekst");
+        Console.WriteLine($"{WarKrotka3.a}, {WarKrotka3.b}, {WarKrotka3.c}");
+        WarKrotka3.a = 123;
 
-        // Uzyskiwanie wartości na podstawie klucza
-        if (dict2.TryGetValue("Alice", out int age))
-        {
-            Console.WriteLine($"Age of Alice: {age}");
-        }
+        (int a, float b, string c) WarKrotka4 = (1, 3.14f, "tekst");
+        Console.WriteLine($"{WarKrotka4.a}, {WarKrotka4.b}, {WarKrotka4.c}");
+       
+        Console.WriteLine($"{WarKrotka1.GetType()}\n{WarKrotka2.GetType()}\n{WarKrotka3.GetType()}\n{WarKrotka4.GetType()}");
+
+
+        // //Zbiór
+        // #pragma warning disable IDE0028
+        // HashSet<int> Zbior1 = new HashSet<int> {7, 2, 3, 4, 5};
+        // HashSet<int> Zbior1b = new () {7, 2, 3, 4, 5};
+
+        // Zbior1.Add(5);
+        // Zbior1.Add(5);
+        // Zbior1.Add(6);
+        // Zbior1.Remove(3);
+
+        // foreach (var item in Zbior1)
+        // {
+        //     Console.Write($"{item}, ");
+        // }
+        // Console.WriteLine();
+
+        // HashSet<string> Zbior2 = ["ala","ma","kota"];
+        // // Console.WriteLine($"{Zbior1},{Zbior2}");
+
+        // foreach (var item in Zbior2)
+        // {
+        //     Console.Write($"{item}, ");
+        // }
+        // Console.WriteLine();
+
+        // //Słownik
+        // // #pragma warning disable IDE0090
+        // Dictionary<string, int> dict1 = new Dictionary<string, int>
+        // {
+        //     { "Alice", 30 },
+        //     { "Bob", 25 },
+        //     { "Charlie", 35 }
+        // };
+
+        // Dictionary<string, int> dict2 = new() { { "Alice", 30 }, { "Bob", 25 }};
+
+        // // Dodawanie elementów do słownika
+        // dict1.Add("David", 40);
+        // dict2.Add("David", 40);
+
+        // // Wyświetlanie zawartości słownika
+        // foreach (var item in dict1)
+        // {
+        //     Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+        // }
+
+        // // Uzyskiwanie wartości na podstawie klucza
+        // if (dict2.TryGetValue("Alice", out int age))
+        // {
+        //     Console.WriteLine($"Age of Alice: {age}");
+        // }
     }
 
 }
