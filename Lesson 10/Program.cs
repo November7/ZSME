8dedefd38e2542dd078145f30b6573b7015491de
November7 <m.kowalski.nov7@outlook.com>
2024-11-19 11:15:27 +0100

    Update

--- Lesson 10/Program.cs
+++ Lesson 10/Program.cs
@@ -1,42 +1,52 @@
-﻿using Fraction;
-using Point;
+﻿class CExample
+{
+    private int a;
+    public int A 
+    {
+        get
+        {
+            return a;
+        } 
+        init
+        {
+            a = value;
+            Console.WriteLine("inicjalizacja A");
+        }
+        // set
+        // {
+        //     if (value < 0)
+        //     {
+        //         a = 0;
+        //     }
+        //     else a = value;
+        // }
+    }
+
+    public CExample()
+    {
+        A = 123;
+        Console.WriteLine("Hi");
+    }
+    ~CExample()
+    {
+        Console.WriteLine("Bye");
+    }
+    public virtual void PrintA()
+    {
+        Console.WriteLine(A);
+    }
+}
+
+
 
 class App
 {
-    static void test()
-    {
-        // CPoint A = new();
-        //     A.X = 23;
-        //     A.Y = 45;
-            CPoint3D B;
-            B = new CPoint3D();
-            B = null;
-            // B.X = 12;
-            // B.Y = 34;
-            // B.Z = 56;
-            // Console.WriteLine($"{A} {B}");
-            // B=new CPoint3D();
-            // B.X = A.X;
-            // Console.WriteLine($"{B}");
-    }
     static void Main()
     {
-        CFraction c = new ();
-        CFraction d = new (1,-2);
-        CFraction e;
-        
-        c.Numerator = 5;
-        c.Denominator = 0;
-        e = c * d;
-        // e++;
-        Console.WriteLine($"{c}, {d}, {e}");
+        CExample example = new CExample();
+        // example.A = 10;
+        example.PrintA();
 
-        for(int i=0;i<100;i++)
-        {
-            test();  
-        }
-        GC.Collect();
-    
+       
     }
-
 }
--- Lesson 11/Program.cs
+++ Lesson 11/Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿using Fraction;
+using Point;
+
+class App
+{
+    static void test()
+    {
+        // CPoint A = new();
+        //     A.X = 23;
+        //     A.Y = 45;
+            CPoint3D B;
+            B = new CPoint3D();
+            B = null;
+            // B.X = 12;
+            // B.Y = 34;
+            // B.Z = 56;
+            // Console.WriteLine($"{A} {B}");
+            // B=new CPoint3D();
+            // B.X = A.X;
+            // Console.WriteLine($"{B}");
+    }
+    static void Main()
+    {
+        CFraction c = new ();
+        CFraction d = new (1,-2);
+        CFraction e;
+        
+        c.Numerator = 5;
+        c.Denominator = 0;
+        e = c * d;
+        // e++;
+        Console.WriteLine($"{c}, {d}, {e}");
+
+        for(int i=0;i<100;i++)
+        {
+            test();  
+        }
+        GC.Collect();
+    
+    }
+
+}
