545fe4e88530e66eff8c250016a13d2b3bd666c9
November7 <m.kowalski.nov7@outlook.com>
2025-01-16 07:21:59 +0100

    Update

---
+++ Examples/2024-2025 3TP/2025.01.15/Program.cs
@@ -0,0 +1,48 @@
+﻿class CExaple
+{
+    public CExaple()
+    {
+        Pole = 0;
+    }
+
+   
+
+    private int pole = 1;
+    public int Pole
+    {
+        get
+        {
+            return pole;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                pole = 0;
+            }
+            else
+            {
+                pole = value;
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{Pole}";  
+
+    }
+}
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CExaple obj = new();
+            obj.Pole = -5;
+            Console.WriteLine(obj);
+        }
+    }
+}
