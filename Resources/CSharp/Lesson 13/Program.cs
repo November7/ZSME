b8414d893f1bcde63f9310a2fb1268d5fe6b8554
November7 <m.kowalski.nov7@outlook.com>
2025-02-11 15:16:56 +0100

    Update

--- Resources/CSharp/Lesson 13/Program.cs
+++ Resources/CSharp/Lesson 13/Program.cs
@@ -3,36 +3,37 @@
     class App
     {
         static void Main()
-        {
-            System.Console.WriteLine(Add(2, 3));
+        {   
+            int x = 3, y = 4;
 
-           
+            System.Console.WriteLine(Add(x, y));
+
             int Add(int a, int b)
             {
                 return a + b;
             }
 
-            int zmienna;
+            int zmienna; //zmienna niezainicjowana
             Init(out zmienna, 101);
 
             System.Console.WriteLine(zmienna);
 
-
             void Init(out int val, int v)
             {
+                //można tylko przypisywać wartość do zmiennej val.
                 if(v<0) val = 0;
                 else if (v>100) val = 100;
                 else val = v;
                 
             }
 
-            int a = 5;
+            int a = 5; //zmienna musi być zainicjowana
             Inc(ref a);
             System.Console.WriteLine(a);
 
             void Inc(ref int a)
             {
-                a++;
+                a++;    //można modyfikować wartość (np. inkrementować)
             }
 
         }
