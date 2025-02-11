5425eb6b5e57758d8da5a88d33ca61326514849b
November7 <m.kowalski.nov7@outlook.com>
2025-02-11 12:26:17 +0100

    Update

---
+++ Resources/CSharp/Lesson 12/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace SettersAndGetters
+{
+
+    class Osoba
+    {
+        private int zachowanie;
+        private int matematyka;
+
+        public int Matematyka
+        {
+            get { return matematyka; }
+            set { 
+                
+                if (value < 1) matematyka = 1;
+                else if (value > 6) matematyka = 6;
+                else  matematyka = value; 
+            }
+        }
+
+        public string Zachowanie
+        {
+            get { return zachowanie.ToString(); }
+            set 
+            { 
+                if (value == "nagana") zachowanie = 1;
+                else if (value == "dostateczne") zachowanie = 2;
+                else if (value == "dobre") zachowanie = 3;
+                else if (value == "bardzo dobre") zachowanie = 4;
+                else if (value == "wzorowe") zachowanie = 5;
+                else if (value == "celujące") zachowanie = 6;
+                else zachowanie = 0;
+            }
+        }
+
+        private string persel;
+
+        public string PESEL
+        {
+            get { return persel; }
+            init { persel = value; }
+        }
+
+        public Osoba(string p)
+        {
+            persel = p;
+        }
+    }
+
+    class App
+    {
+        static void Main()
+        {
+            Osoba oceny = new Osoba("12345678901");
+            oceny.Matematyka = 5;
+            oceny.Zachowanie = "dobre";
+            // oceny.PESEL = "12345678901";
+
+            System.Console.WriteLine("Ocena z matematyki: " + oceny.Matematyka);
+            System.Console.WriteLine("Ocena z zachowania: " + oceny.Zachowanie);
+            System.Console.WriteLine("PESEL: " + oceny.PESEL);
+
+        }
+    }
+
+}
---
+++ Resources/CSharp/Lesson 13/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace LocalFunctions
+{
+    class App
+    {
+        static void Main()
+        {
+            System.Console.WriteLine(Add(2, 3));
+
+           
+            int Add(int a, int b)
+            {
+                return a + b;
+            }
+
+            int zmienna;
+            Init(out zmienna, 101);
+
+            System.Console.WriteLine(zmienna);
+
+
+            void Init(out int val, int v)
+            {
+                if(v<0) val = 0;
+                else if (v>100) val = 100;
+                else val = v;
+                
+            }
+
+            int a = 5;
+            Inc(ref a);
+            System.Console.WriteLine(a);
+
+            void Inc(ref int a)
+            {
+                a++;
+            }
+
+        }
+    }
+}
