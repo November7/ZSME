f90ffe14802a2884997fb95b72f8b86416ab5250
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2025-02-24 16:19:01 +0100

    Update

--- Examples/2024-2025 1TP/20250224.py
+++ Examples/2024-2025 1TP/20250224.py
@@ -16,6 +16,18 @@ def Witaj(imie:str = "", nazwisko:str = "") -> None: #definicja funkcji
     ...
     print(f"Witaj {imie} {nazwisko}") #wyświetlenie napisu
     
+
+#Ćwiczenie
+
+def Dodawanie(a, b):
+    return a+b
+
+def Odejmowanie(a, b):
+    return a-b
+
+def Mnozenie(a, b):
+    return a * b
+
 def Dzielenie(a:float, b:float): #definicja funkcji
     if a > 0 and b == 0:
         return "Infinity"
@@ -25,8 +37,20 @@ def Dzielenie(a:float, b:float): #definicja funkcji
         return "NaN"
     return a/b #zwrócenie wyniku dzielenia
 
+def NWD(a, b):
+    while a != b:
+        if a > b:
+            a -= b
+        else:
+            b -= a
+    return a
 
-print(Dzielenie(3,0))
-print(Dzielenie(-13,0))
-print(Dzielenie(0,0))
-print(Dzielenie(20,10))
+liczba_a = int(input("Podaj pierwszą liczbę: "))
+liczba_b = int(input("Podaj pierwszą liczbę: "))
+
+print(f"Wynik dodawania {liczba_a} i {liczba_b} wynosi {Dodawanie(liczba_a, liczba_b)}")
+print(f"Wynik odejmowania {liczba_a} i {liczba_b} wynosi {Odejmowanie(liczba_a, liczba_b)}")
+print(f"Wynik mnożenia {liczba_a} i {liczba_b} wynosi {Mnozenie(liczba_a, liczba_b)}")
+print(f"Wynik dzielenia {liczba_a} i {liczba_b} wynosi {Dzielenie(liczba_a, liczba_b)}")
+print(f"Największy wspólny dzielnik {liczba_a} i {liczba_b} wynosi {NWD(liczba_a, liczba_b)}")
+
