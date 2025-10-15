ef09f64b44b5da3b93c654f09bd0f6ee23ea7814
November7 <m.kowalski.nov7@outlook.com>
2025-10-15 23:24:49 +0200

    Update

--- Resources/Python/Lesson 1/Lesson 1.1.py
+++ Resources/Python/Lesson 1/Lesson 1.1.py
@@ -1,6 +1,42 @@
-def Example1(a,b,c,/,*,d,e,f):
-    print(f'{a = }, {b = }, {c = }, {d = }, {e = }, {f = }')
+# Definiowanie funkcji w python
+
+# Przykładowa funkcja (która nic nie robi)
+def DoNothing():
+    ...
+
+# Wywołanie funkcji
+DoNothing()
+
+# Funkcja z parametrem
+# Wpython możliwe jest określenie typu parametru
+# oraz typu zwracanego przez funkcję
+# taka operacja nazywa się "type hinting"
+# (w rzeczywistości Python i tak nie wymusza typów)
+
+def PrintText(text: str) -> None:
+    print(text)
+
+# Wywołanie funkcji z parametrem
+PrintText("Hello World!")
 
 
+# Funkcja z parametrem domyślnym
+def PrintTextWithPrefix(text: str, prefix: str = "Info: ") -> None:
+    print(prefix + text)
 
-Example1(1,2,3,d=4,e=5,f=6)
+# Wywołania funkcji z i bez parametru domyślnego
+PrintTextWithPrefix("Hello World!")
+
+# Wywołanie z nadpisaniem parametru domyślnego
+PrintTextWithPrefix("Hello World!", "Warning: ")
+
+# Funkcja zwracająca wartość
+def Add(a: int, b: int) -> int:
+    return a + b
+
+# Wywołanie funkcji i przypisanie wyniku do zmiennej
+result = Add(5, 3)
+print(result)
+
+# Wywołanie funkcji bez przypisywania wyniku do zmiennej
+print(Add(10, 20))
