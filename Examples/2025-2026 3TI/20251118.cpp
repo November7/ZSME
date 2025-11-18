b3188ba8993982fbd4556293435535acfe90c1cf
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2025-11-18 15:18:55 +0100

    Update

--- Examples/2025-2026 3TI/20251118.cpp
+++ Examples/2025-2026 3TI/20251118.cpp
@@ -6,14 +6,16 @@ using namespace std;
 
 int main()
 {
+    
     srand(time(NULL));
     // cout<<RAND_MAX<<endl;
+
     for (int i = 0 ; i<10 ; i++) rand(); // <0, RAND_MAX)
   
     int dane[10];
 
     for (int i = 0 ; i<10 ; i++)
-        dane[i] = 100 * rand() / RAND_MAX; // <0, 100)
+        dane[i] = int(100 * float(rand())/RAND_MAX); //<0, 100)
 
     for (int i = 0 ; i<10 ; i++)
         cout << dane[i] <<", ";
