f11f7559a28d4edc70d116d8873f41a26d2ee6e8
November7 <m.kowalski.nov7@outlook.com>
2026-04-14 11:54:19 +0200

    Update

---
+++ Examples/Python/klasy.py
@@ -0,0 +1,88 @@
+class Vehicle:
+    count = 0  # pole statyczne (klasy)
+
+    def __init__(self, brand, model, year, mileage=0):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.__mileage = mileage  # pole prywatne
+
+        Vehicle.count += 1  # inkrementacja licznika obiektów
+
+    @property
+    def mileage(self):
+        return self.__mileage
+
+    @mileage.setter
+    def mileage(self, value):
+        if value < self.__mileage:
+            raise ValueError("Przebieg nie może zostać zmniejszony!")
+        self.__mileage = value
+
+    
+    def drive(self, km):
+        if km < 0:
+            raise ValueError('Nie można jeschać ujemną ilość km')
+        self.__mileage += km
+
+    def info(self):
+        return f"{self.brand} {self.model} ({self.year}), przebieg: {self.__mileage} km"
+
+    # @staticmethod
+    def get_count():
+        """Zwraca liczbę utworzonych pojazdów."""
+        return Vehicle.count
+
+    @classmethod
+    def from_string(cls, text):
+        """Tworzy pojazd z tekstu: 'Toyota;Corolla;2018;120000'."""
+        brand, model, year, mileage = text.split(";")
+        return cls(brand, model, int(year), int(mileage))
+
+
+class Car(Vehicle):
+    def __init__(self, brand, model, year, doors, mileage=0):
+        super().__init__(brand, model, year, mileage)
+        self.doors = doors
+
+    def info(self):
+        base = super().info()
+        return f"{base}, drzwi: {self.doors}"
+
+
+class Motorcycle(Vehicle):
+    def __init__(self, brand, model, year, moto_type, mileage=0):
+        super().__init__(brand, model, year, mileage)
+        self.moto_type = moto_type
+
+    def info(self):
+        base = super().info()
+        return f"{base}, typ motocykla: {self.moto_type}"
+
+
+def show_vehicle(v):
+    print(v.info())
+
+
+# --- TEST ---
+car = Car("Toyota", "Corolla", 2018, 5, 120000)
+moto = Motorcycle("Yamaha", "MT-07", 2020, "sport", 15000)
+
+car.drive(150)
+# car._Vehicle__mileage = 0
+# car.__dict__["_Vehicle__mileage"] = 0
+moto.drive(40)
+
+
+show_vehicle(car)
+show_vehicle(moto)
+
+print("Liczba pojazdów:", Vehicle.get_count())
+
+# Tworzenie z tekstu
+v2 = Vehicle.from_string("Honda;Civic;2015;90000")
+show_vehicle(v2)
+print("Liczba pojazdów:", Vehicle.get_count())
+
+
+
